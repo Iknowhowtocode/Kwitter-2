aa6337f6061ae737c693cf6fc708f06d17df8aca
Iknowhowtocode <110275949+Iknowhowtocode@users.noreply.github.com>
2023-05-28 08:34:52 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCE9J_mx2oc3a6E7wKO7K27TOo7v2WXBxY",
+    authDomain: "kwitter-project-1797b.firebaseapp.com",
+    projectId: "kwitter-project-1797b",
+    storageBucket: "kwitter-project-1797b.appspot.com",
+    messagingSenderId: "420355529649",
+    appId: "1:420355529649:web:09419b37b6c3394fe57b90",
+    measurementId: "G-5NKKR7VLQL"
+  };
